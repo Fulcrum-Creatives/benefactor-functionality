aba341f89fe0a1be1cc4a9b6ccee516eb78832d9
jawittdesigns <jawittdesigns@gmail.com>
2015-02-17 10:16:10 -0500

    Added Github Updater Support

--- benefactor.php
+++ benefactor.php
@@ -23,6 +23,8 @@
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       benefactor
  * Domain Path:       /languages
+ * GitHub Plugin URI: https://github.com/Fulcrum-Creatives/benefactor-functionality
+ * GitHub Branch:     master
  */
 
 // If this file is called directly, abort.
